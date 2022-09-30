120aa17a142176e04bc024ba82c404570db89934
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-09-30 11:13:28 -0400

    Grid sorted out. Play button sort of working.

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', event => {
 
 
 function getDataFromItunes(){    
-    let url = `https://itunes.apple.com/search?term=${searchValue}&limit=25`
+    let url = `https://itunes.apple.com/search?term=${searchValue}&limit=24`
     console.log(url)
 
     fetch(url, {
@@ -26,35 +26,44 @@ function getDataFromItunes(){
     .then( response => { return response.json() } )
     .then( json => {
         console.log(json);
-
         let finalHTML = '';
 
-        json.results.forEach( song => {
+        if(json.resultCount === 0)
+        {
+            alert("No result found.");
 
-            finalHTML += 
-            `
-            <div class="row">
-                <div class="col s4 m4 l4">
-                    <div class="card">
-                        <div class="card-image">
+        }else{
+
+            json.results.forEach( song => {
+
+                finalHTML += 
+                `
+                <div class="row ">
+                    <div class="col s4 m4 l4">
+                        <div class="card large">
+                            <div class="card-image">
                             <img src=${song.artworkUrl100.replaceAll('100x100bb', '300x300bb')}>
                             <audio id=${song.trackId} src=${song.previewUrl} class="songAudio" preload="auto"></audio>
-                                <button onclick="document.getElementById(${song.trackId}).play();" class="btn-floating halfway-fab waves-effect waves-light red" type="submit"><i class="material-icons">play_arrow</i></button>
-                        </div>
-                                               
-                        <div class="card-content">
-                            <span class="card-title">${song.trackName}</span>
-                            <p>${song.artistName}</p>
+                            <button onclick="document.querySelectorAll('audio').forEach(el => el.pause()); document.getElementById(${song.trackId}).play();" class="btn-floating btn-large halfway-fab waves-effect waves-light red" type="submit"><i class="material-icons">play_arrow</i></button>
+                            
+                        </div>    
+                                                            
+                            <div class="card-content">
+                                <span class="card-title">${song.trackName}</span>
+                                
+                                <p id="artistName">${song.artistName}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                
+            `     
             
-           `     
-          
-            
-        })
-        results.innerHTML = finalHTML;
+                
+            })
+            results.innerHTML = finalHTML;
+        }
     })
     .catch( error => console.log(error))
 }
+

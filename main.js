77e0d809ea02650763672b8c8024db6450292e7c
Marcus Nogueira <marcusnogueira23@gmail.com>
2022-09-30 09:25:02 -0400

    Search results populated. Audio sort of working.

--- main.js
+++ main.js
@@ -1,17 +1,60 @@
+let form = document.getElementById("searchBar");
+let input = document.getElementById("artistSearch");
+let button = document.getElementById("searchButton");
+let results = document.getElementById("results");
+let searchValue;
 
-let url = 'https://itunes.apple.com/search?term=jack+johnson&entity=music&limit=25'
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    searchValue = input.value.replaceAll(' ','+');
+    getDataFromItunes();
+} )
 
 
-fetch(url, {
-    method:'GET',
-    headers: {'Content-Type': 'application/json'},
 
-})
-.then(function(response){
-    return response.json();
-})
-.then(function(parsedResponse){
-    for(let artwork of parsedResponse.data){
-        console.log(`${artwork.title} by ${artwork.artist_title}`)
-    }
-})
+
+function getDataFromItunes(){    
+    let url = `https://itunes.apple.com/search?term=${searchValue}&limit=25`
+    console.log(url)
+
+    fetch(url, {
+        method:'GET',
+        headers: {'Content-Type': 'application/json'},
+
+    })
+    .then( response => { return response.json() } )
+    .then( json => {
+        console.log(json);
+
+        let finalHTML = '';
+
+        json.results.forEach( song => {
+
+            finalHTML += 
+            `
+            <div class="row">
+                <div class="col s4 m4 l4">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src=${song.artworkUrl100.replaceAll('100x100bb', '300x300bb')}>
+                            <audio id=${song.trackId} src=${song.previewUrl} class="songAudio" preload="auto"></audio>
+                                <button onclick="document.getElementById(${song.trackId}).play();" class="btn-floating halfway-fab waves-effect waves-light red" type="submit"><i class="material-icons">play_arrow</i></button>
+                        </div>
+                                               
+                        <div class="card-content">
+                            <span class="card-title">${song.trackName}</span>
+                            <p>${song.artistName}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+           `     
+          
+            
+        })
+        results.innerHTML = finalHTML;
+    })
+    .catch( error => console.log(error))
+}
